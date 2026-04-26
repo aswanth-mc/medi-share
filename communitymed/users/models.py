4df47fe9c2ec743e4059e0fb010dd530e1bee041
aswanth-mc <mcaswanth5@gmail.com>
2026-04-26 22:02:47 +0530

    fix : fix the migration errors

--- communitymed/users/models.py
+++ communitymed/users/models.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 class User(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Admin'),
-        ('palliative_care', 'Palliative Care'),
-        ('user', 'User')
+        ('unit', 'Palliative Unit'),
+        ('user', 'User'),
     )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
