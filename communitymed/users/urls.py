e85eef4673e92f4a17ed5acba1b2731314dc3709
aswanth-mc <mcaswanth5@gmail.com>
2026-04-27 10:35:04 +0530

    feat(admin) : add admin dashboard url

--- communitymed/users/urls.py
+++ communitymed/users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.user_login, name='login'),
     #path('register/', views.register, name='register'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     
 ]
