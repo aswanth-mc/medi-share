9998bbe7c02dd9503a7c83e4294638710712b7a4
aswanth-mc <mcaswanth5@gmail.com>
2026-04-27 07:25:27 +0530

    fix(auth) : replace with email

--- communitymed/users/views.py
+++ communitymed/users/views.py
@@ -4,20 +4,28 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
+    if request.method == "POST":
+        email = request.POST['email']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user :
-            login(request, user)
-            if user.role == 'admin':
-                return redirect('admin_dashboard')
-            elif user.role == 'unit':
-                return redirect('unit_dashboard')
-            else: 
-                return redirect('user_dashboard')
-        else:
-            return render(request, '/login.html', {'error': 'Invalid username or password'})
-        
+
+        user_obj = User.objects.filter(email=email).first()
+
+        if user_obj:
+            user = authenticate(request, username=user_obj.username, password=password)
+
+            if user:
+                login(request, user)
+
+             
+                if user.role == 'admin':
+                    return redirect('admin_dashboard')
+                elif user.role == 'unit':
+                    return redirect('unit_dashboard')
+                else:
+                    return redirect('home')
+
+        return render(request, 'login.html', {'error': 'Invalid credentials'})
+
     return render(request, 'login.html')
