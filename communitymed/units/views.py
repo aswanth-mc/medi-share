0615f4e46112257ab3bd9ac09c82d619082375a1
aswanth-mc <mcaswanth5@gmail.com>
2026-04-28 11:30:26 +0530

    feat : add palliative vieew

--- communitymed/units/views.py
+++ communitymed/units/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from .models import PalliativeUnit
 
-# Create your views here.
+User = get_user_model()
+
+def register_unit(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = User.objects.create_user(
+            username=email,
+            email=email,
+            password=password,
+            role='unit'
+        )
+
+        PalliativeUnit.objects.create(
+            user=user,
+            name=request.POST['name'],
+            license_number=request.POST['license_number'],
+            license_file=request.FILES['license_file']
+        )
+
+        return redirect('login')
+
+    return render(request, 'register_unit.html')
