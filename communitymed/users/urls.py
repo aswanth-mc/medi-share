cf4ae2a96cb8b3fadbe83d1a869b3fc3e7499550
aswanth-mc <mcaswanth5@gmail.com>
2026-05-02 16:51:27 +0530

    feat : add usere registration urls

--- communitymed/users/urls.py
+++ communitymed/users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.user_login, name='login'),
     path('register-choice/', views.register_choice, name='register_choice'),
+    path('register-user/', views.register_user, name='register_user'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    
 ]
