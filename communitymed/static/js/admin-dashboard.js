0bccb3adc074aa8755612de9872d3a71ad999c31
aswanth-mc <mcaswanth5@gmail.com>
2026-05-05 21:49:38 +0530

    feat : add dialog type box

--- communitymed/static/js/admin-dashboard.js
+++ communitymed/static/js/admin-dashboard.js
@@ -1,16 +1,15 @@
 function openModal(id) {
-    document.getElementById("modal-" + id).style.display = "block";
+    document.getElementById("modal-" + id).classList.add("show");
 }
 
 function closeModal(id) {
-    document.getElementById("modal-" + id).style.display = "none";
+    document.getElementById("modal-" + id).classList.remove("show");
 }
 
-// Close modal when clicking outside
 window.onclick = function(event) {
     document.querySelectorAll('.modal').forEach(modal => {
         if (event.target === modal) {
-            modal.style.display = "none";
+            modal.classList.remove("show");
         }
     });
 };
