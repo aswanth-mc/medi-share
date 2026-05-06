cfc0fa05250ab217f5699e9df378ed5ddb605220
aswanth-mc <mcaswanth5@gmail.com>
2026-05-06 09:48:46 +0530

    feat : add medicine donation model

--- communitymed/inventory/models.py
+++ communitymed/inventory/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.config import settings
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class MedicineDonation(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+        ('collected', 'Collected')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    medicine_name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    category = models.CharField(max_length=100)
+    expiry_date = models.DateField()
+    image = models.ImageField(upload_to='medicine_images/')
+    pickup_date = models.DateField()
+    pickup_time = models.TimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.medicine_name
+    
