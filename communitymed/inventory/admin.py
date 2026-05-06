5e638133c7b31481d9449a412abc6babcaf7692a
aswanth-mc <mcaswanth5@gmail.com>
2026-05-06 09:59:18 +0530

    feat  : register the models

--- communitymed/inventory/admin.py
+++ communitymed/inventory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MedicineDonation, UnitInventory
 
 # Register your models here.
+admin.site.register(MedicineDonation)
+admin.site.register(UnitInventory)
