83ea2c496ec53fcfaf4d6503fc9aafbf9d44f5d7
aswanth-mc <mcaswanth5@gmail.com>
2026-05-07 19:38:28 +0530

    feat(collect donation) : add donation collect view

--- communitymed/inventory/views.py
+++ communitymed/inventory/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from inventory.models import MedicineDonation, UnitInventory
 
 # Create your views here.
+@login_required
+def collect_donation(request, donation_id):
+    if request.user.role != 'unit':
+        return redirect('home')
+
+    donation = MedicineDonation.objects.get(id=donation_id)
+
+    donation.status = 'collected'
+    donation.save()
+
+    UnitInventory.objects.create(
+        unit=request.user,
+        donation=donation,
+        name=donation.name,
+        quantity=donation.quantity,
+        category=donation.category,
+        expiry_date=donation.expiry_date
+    )
+
+    return redirect('unit_dashboard')
