246c2437d7b845db06ddb2af26f5bbec4924023f
aswanth-mc <mcaswanth5@gmail.com>
2026-05-19 23:12:38 +0530

    feat : add url for donation view

--- communitymed/units/urls.py
+++ communitymed/units/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.register_unit, name='register_unit'),
     path('approve/<int:unit_id>/', views.approve_unit, name='approve_unit'),
     path('dashboard/', views.unit_dashboard, name='unit_dashboard'),
+    path('donations/', views.unit_donations, name='unit_donations'),
 ]
