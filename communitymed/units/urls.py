5005b67e5cf74166ce14143f3739f3b82d8dc6bb
aswanth-mc <mcaswanth5@gmail.com>
2026-05-20 11:21:25 +0530

    fix : reaname the url names

--- communitymed/units/urls.py
+++ communitymed/units/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('approve/<int:unit_id>/', views.approve_unit, name='approve_unit'),
     path('dashboard/', views.unit_dashboard, name='unit_dashboard'),
     path('donations/', views.unit_donations, name='unit_donations'),
+    path('approve_donation/<int:donation_id>/',views.approve_donation,name='approve_donation'),
+    path('reject_donation/<int:donation_id>/',views.reject_donation,name='reject_donation'),
+    path('collect_donation/<int:donation_id>/',views.collect_donation,name='collect_donation'),
 ]
