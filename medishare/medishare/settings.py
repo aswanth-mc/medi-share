4ff7bf783d7ac41d6b33da9be75dc7a7f79e2181
aswanth-mc <mcaswanth5@gmail.com>
2026-05-22 19:42:57 +0530

    feat : start a new app for admin and config it

--- medishare/medishare/settings.py
+++ medishare/medishare/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
-    'unit'
+    'unit',
+    'customadmin',
+    
 ]
 AUTH_USER_MODEL = 'user.User'
 
