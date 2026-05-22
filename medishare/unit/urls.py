15908f39bb26a9de3375f46e7c05c13ceb044181
aswanth-mc <mcaswanth5@gmail.com>
2026-05-22 23:49:07 +0530

    feat : add unit dashboard

--- medishare/unit/urls.py
+++ medishare/unit/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_unit, name='register_unit'),
+    path('dashboard/', views.unit_dashboard, name='unit_dashboard'),
     
 ]
