5924627be0124e0e86b441197fe45baa29e19893
aswanth-mc <mcaswanth5@gmail.com>
2026-05-22 22:30:50 +0530

    feat : add user registrations

--- medishare/user/models.py
+++ medishare/user/models.py
@@ -1,46 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
+from django.utils import timezone
 
 
-ROLE_CHOICES = (
-    ('admin', 'Admin'),
-    ('user', 'User'),
-    ('unit', 'Palliative Unit'),
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+        ('unit', 'Palliative Unit'),
+    )
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+
+    email = models.EmailField(unique=True)
+
+    phone = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True
+    )
+
+    location_name = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+
+    latitude = models.FloatField(
+        blank=True,
+        null=True
+    )
+
+    longitude = models.FloatField(
+        blank=True,
+        null=True
+    )
+
+    created_at = models.DateTimeField(
+    default=timezone.now
 )
 
-role = models.CharField(
-    max_length=10,
-    choices=ROLE_CHOICES,
-    default='user'
-)
-
-email = models.EmailField(
-    unique=True
-)
-
-phone = models.CharField(
-    max_length=15,
-    blank=True,
-    null=True
-)
-
-location_name = models.CharField(
-    max_length=255,
-    blank=True,
-    null=True
-)
-
-latitude = models.FloatField(
-    blank=True,
-    null=True
-)
-
-longitude = models.FloatField(
-    blank=True,
-    null=True
-)
-def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
