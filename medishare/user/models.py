46bf95b3f95f79a1e67ac6070ea3e04a8ce9d482
aswanth-mc <mcaswanth5@gmail.com>
2026-05-22 22:01:27 +0530

    feat : add some extara models

--- medishare/user/models.py
+++ medishare/user/models.py
@@ -5,16 +5,42 @@ from django.contrib.auth.models import AbstractUser
 
 
 
-class User(AbstractUser):
+ROLE_CHOICES = (
+    ('admin', 'Admin'),
+    ('user', 'User'),
+    ('unit', 'Palliative Unit'),
+)
 
-    ROLE_CHOICES = (
-        ('admin', 'Admin'),
-        ('user', 'User'),
-        ('unit', 'Unit'),
-    )
+role = models.CharField(
+    max_length=10,
+    choices=ROLE_CHOICES,
+    default='user'
+)
 
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    phone = models.CharField(max_length=15,blank=True,null=True)
+email = models.EmailField(
+    unique=True
+)
 
-    def __str__(self):
-        return self.username
+phone = models.CharField(
+    max_length=15,
+    blank=True,
+    null=True
+)
+
+location_name = models.CharField(
+    max_length=255,
+    blank=True,
+    null=True
+)
+
+latitude = models.FloatField(
+    blank=True,
+    null=True
+)
+
+longitude = models.FloatField(
+    blank=True,
+    null=True
+)
+def __str__(self):
+    return self.username
