752c86b67180f8ab74f705c44dc5f7498a6e7749
Aswanth M.C <mcaswanth5@gmail.com>
2026-05-22 10:07:49 +0530

    Delete communitymed directory

--- communitymed/inventory/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MedicineDonation, UnitInventory
-
-# Register your models here.
-admin.site.register(MedicineDonation)
-admin.site.register(UnitInventory)
--- communitymed/inventory/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('collect-donation/<int:donation_id>/', views.collect_donation, name='collect_donation'),
-]
