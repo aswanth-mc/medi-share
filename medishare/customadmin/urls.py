1f06317c814f3380a1efc01992919b0935740cf5
aswanth-mc <mcaswanth5@gmail.com>
2026-05-23 11:56:21 +0530

    feat : add approve unit logic

--- medishare/customadmin/urls.py
+++ medishare/customadmin/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('approve-unit/<int:user_id>/', views.approve_unit, name='approve_unit'),
+    path('reject-unit/<int:user_id>/', views.reject_unit, name='reject_unit'),
 ]
--- medishare/customadmin/views.py
+++ medishare/customadmin/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect 
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth import get_user_model
+from unit .models import PalliativeUnit
 # Create your views here.
 
+User = get_user_model()
+
+@login_required
 def admin_dashboard(request):
-    return render(request, 'admin_dashboard.html')
+    if request.user.role != 'admin':
+        return redirect('login')
+    pending_units = User.objects.filter(role='unit', verification_status='pending')
+    context = {'pending_units': pending_units}
+    return render(request, 'admin_dashboard.html', context)
+
+@login_required
+def approve_unit(request, unit_id):
+    if request.user.role != 'admin':
+        return redirect('login')
+
+    try:
+        unit = User.objects.get(id=unit_id, role='unit')
+        unit.verification_status = 'approved'
+        unit.save()
+    except User.DoesNotExist:
+        pass
+
+    return redirect('admin_dashboard')
+
+@login_required
+def reject_unit(request, unit_id):
+    if request.user.role != 'admin':
+        return redirect('login')
+
+    try:
+        unit = User.objects.get(id=unit_id, role='unit')
+        unit.verification_status = 'rejected'
+        unit.save()
+    except User.DoesNotExist:
+        pass
+
+    return redirect('admin_dashboard')
