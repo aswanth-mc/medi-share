46df2e7530a08cd7c903bd470b378b01aaafceef
aswanth-mc <mcaswanth5@gmail.com>
2026-05-23 10:22:28 +0530

    feat : add is verified filed

--- medishare/user/models.py
+++ medishare/user/models.py
@@ -11,39 +11,14 @@ class User(AbstractUser):
         ('unit', 'Palliative Unit'),
     )
 
-    role = models.CharField(
-        max_length=10,
-        choices=ROLE_CHOICES,
-        default='user'
-    )
-
+    role = models.CharField( max_length=10,choices=ROLE_CHOICES,default='user')
     email = models.EmailField(unique=True)
-
-    phone = models.CharField(
-        max_length=15,
-        blank=True,
-        null=True
-    )
-
-    location_name = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True
-    )
-
-    latitude = models.FloatField(
-        blank=True,
-        null=True
-    )
-
-    longitude = models.FloatField(
-        blank=True,
-        null=True
-    )
-
-    created_at = models.DateTimeField(
-    default=timezone.now
-)
+    phone = models.CharField(max_length=15,blank=True,null=True)
+    location_name = models.CharField(max_length=255,blank=True,null=True)
+    latitude = models.FloatField(blank=True,null=True)
+    longitude = models.FloatField(blank=True,null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
