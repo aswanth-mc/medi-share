b841614757fad0ceef990c0d728318a5050444ab
aswanth-mc <mcaswanth5@gmail.com>
2026-05-23 22:34:53 +0530

    feat (inventory) :add inventory logic

--- medishare/inventory/urls.py
+++ medishare/inventory/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path(
+        'receive/<int:donation_id>/',
+        views.receive_donation,
+        name='receive_donation'
+    ),
 ]
--- medishare/inventory/views.py
+++ medishare/inventory/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.contrib.auth.decorators import login_required
+
+from donations.models import Donation
+from inventory.models import Inventory
 # Create your views here.
+
+
+
+@login_required
+def receive_donation(request, donation_id):
+
+    donation = Donation.objects.get(id=donation_id)
+
+    Inventory.objects.create(
+        unit=donation.unit,
+        medicine_name=donation.medicine_name,
+        quantity=donation.quantity,
+        expiry_date=donation.expiry_date,
+        medicine_image=donation.medicine_image,
+        description=donation.description
+    )
+
+    donation.status = 'received'
+
+    donation.save()
+
+    return redirect('incoming_donations')
