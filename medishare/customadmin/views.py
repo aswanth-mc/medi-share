82bc2e3baf9796eedd87e4f75e4bf23bca2cddbf
aswanth-mc <mcaswanth5@gmail.com>
2026-05-23 10:14:10 +0530

    fix (admin_dashboard) : currect the dashboard path

--- medishare/customadmin/views.py
+++ medishare/customadmin/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def admin_dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'admin_dashboard.html')
